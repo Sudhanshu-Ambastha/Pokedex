d19a3d6c7d73e6f199db8690cd1139f171c79da3
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-09-21 23:14:19 +0530

    🧪test

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ List2: from 10001 - 10277
 
 for alolan forms- ex= raichu-alola 
 
-[Running Model](https://onecompiler.com/html/42qytz7sa)
+[Running Model](https://onecompiler.com/html/42sv3n8c7)
--- script.js
+++ script.js
@@ -1,73 +1,36 @@
-const pokemonID = document.getElementById('pokemon-id');
-const pokemonName = document.getElementById('pokemon-name');
-const spriteContainer = document.getElementById('sprite-container');
-const types = document.getElementById('types');
-const height = document.getElementById('height');
-const weight = document.getElementById('weight');
-const hp = document.getElementById('hp');
-const attack = document.getElementById('attack');
-const defense = document.getElementById('defense');
-const specialAttack = document.getElementById('special-attack');
-const specialDefense = document.getElementById('special-defense');
-const speed = document.getElementById('speed');
-const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
+const search_term = document.getElementById('search_q')
+const search_btn = document.getElementById('search-btn')
 
-const getPokemon = async () => {
-  try {
-    const pokemonNameOrId = searchInput.value.toLowerCase();
-    const response = await fetch(
-      `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`
-    );
-    const data = await response.json();
 
-    // Set Pokémon info
-    pokemonName.textContent = `${data.name.toUpperCase()}`;
-    pokemonID.textContent = `#${data.id}`;
-    weight.textContent = `Weight: ${data.weight}`;
-    height.textContent = `Height: ${data.height}`;
-    spriteContainer.innerHTML = `
-      <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
-    `;
+// api https://pokeapi.co/docs/v2#pokemon
+const getPokemonData = async term => {
+    document.getElementById('show_error').classList.remove('show')
+    document.getElementById('show_error').classList.add('hidden')
+        
+    const url = `https://pokeapi.co/api/v2/pokemon/${term}`
+    const response = await fetch(url)
 
-    // Set stats
-    hp.textContent = data.stats[0].base_stat;
-    attack.textContent = data.stats[1].base_stat;
-    defense.textContent = data.stats[2].base_stat;
-    specialAttack.textContent = data.stats[3].base_stat;
-    specialDefense.textContent = data.stats[4].base_stat;
-    speed.textContent = data.stats[5].base_stat;
+    if(response.status == 404 || response.statusText == 'Not Found'){
+        document.getElementById('show_error').classList.add('show')
+        document.getElementById('show_error').classList.remove('hidden')
+        return
+    }
 
-    // Set types
-    types.innerHTML = data.types
-      .map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`)
-      .join('');
-  } catch (err) {
-    resetDisplay();
-    alert('Pokémon not found');
-    console.log(`Pokémon not found: ${err}`);
-  }
-};
+    const pokemon = await response.json()
+    debugger
 
-const resetDisplay = () => {
-  const sprite = document.getElementById('sprite');
-  if (sprite) sprite.remove();
+    // update ui with data 
+    document.getElementById('update_img').setAttribute('src', pokemon.sprites.other.dream_world.front_default)
+    document.getElementById('update_name').innerHTML = pokemon.name
+    document.getElementById('update_candy_title').innerHTML = `${pokemon.name} Candy`
+    document.getElementById('update_hp').innerHTML = `HP ${Math.floor((Math.random() * pokemon.stats[0].base_stat) + 1)}/${pokemon.stats[0].base_stat}`
+    document.getElementById('update_cp').innerHTML = `XP ${pokemon.base_experience}`
+    document.getElementById('update_type').innerHTML = `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`
+    document.getElementById('update_weight').innerHTML = `${pokemon.weight}kg`
+    document.getElementById('update_height').innerHTML = `0.${pokemon.height}m`
+    document.getElementById('update_stardust').innerHTML = Math.floor((Math.random() * 10000) + 1)
+    document.getElementById('update_candy').innerHTML = Math.floor((Math.random() * 200) + 1)
 
-  // reset stats
-  pokemonName.textContent = '';
-  pokemonID.textContent = '';
-  types.innerHTML = '';
-  height.textContent = '';
-  weight.textContent = '';
-  hp.textContent = '';
-  attack.textContent = '';
-  defense.textContent = '';
-  specialAttack.textContent = '';
-  specialDefense.textContent = '';
-  speed.textContent = '';
-};
+}
 
-searchForm.addEventListener('submit', e => {
-  e.preventDefault();
-  getPokemon();
-});
+search_btn.addEventListener('click', () => getPokemonData(search_term.value))
